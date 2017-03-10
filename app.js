c96b1abda2511be98bd91d36d37cfeb9ec6e65e8
Tina Lam <tina.lam516@gmail.com>
2017-03-10 18:25:00 -0500

    up to getters

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
+var chalk = require('chalk');
+var models = require('./models');
 const app = require('express')();
 const env = nunjucks.configure('views', {noCache: true});
 
-// have res.render work with html files
+
+
 app.set('view engine', 'html');
-// when res.render works with html files, have it use nunjucks to do so
 app.engine('html', nunjucks.render);
+
+models.User.sync({})
+.then(function () {
+    return models.Page.sync({})
+})
+.then(function () {
+    app.listen(3000, function () {
+        console.log(chalk.bgCyan('hay im hur on port 3000'));
+    });
+})
+.catch(console.error);
